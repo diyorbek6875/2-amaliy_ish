632576b5572f2e016c7bc440e99e6c05f84fb8fb
Diyorbek <129488800+diyorbek6875@users.noreply.github.com>
2024-03-12 10:30:23 +0500

    Create amaliy3.dart

---
+++ amaliy3.dart
@@ -0,0 +1,13 @@
+void main() {
+  int son = 7; // Misol uchun son
+
+  if (son % 4 == 1) {
+    print('Birinchi honaga tegishli');
+  } else if (son % 4 == 2) {
+    print('Ikkinchi honaga tegishli');
+  } else if (son % 4 == 3) {
+    print('Uchinchi honaga tegishli');
+  } else {
+    print('Toʻrtinchi honaga tegishli');
+  }
+}
